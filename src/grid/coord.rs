b4952d9365bc91446d7af20d7e28b52b387a1367
Stephen Sherratt <stephen@sherra.tt>
2016-08-21 00:07:23 +1000

    Removed unused code

--- src/grid/coord.rs
+++ src/grid/coord.rs
@@ -1,19 +1,2 @@
 use geometry::Vector2;
-
 pub type Coord = Vector2<isize>;
-
-impl Coord {
-    pub fn wrapping_increment_in_place(&mut self, width: usize) {
-        self.x += 1;
-        if self.x == width as isize {
-            self.x = 0;
-            self.y += 1;
-        }
-    }
-
-    pub fn wrapping_increment(&self, width: usize) -> Coord {
-        let mut ret = *self;
-        ret.wrapping_increment_in_place(width);
-        ret
-    }
-}
