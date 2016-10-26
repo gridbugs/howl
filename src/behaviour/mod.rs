7d2bd584b16e267a5ad9f2c2c0f9192738274bb0
Stephen Sherratt <stephen@sherra.tt>
2016-10-27 00:59:27 +1100

    Start of new behaviour graph implementation

--- src/behaviour/mod.rs
+++ src/behaviour/mod.rs
@@ -4,3 +4,5 @@ mod tests;
 mod behaviour;
 pub use self::behaviour::{Graph, NodeIndex, State, LeafResolution, CheckResolution, LeafFnBox,
                           CheckFnBox, CollectionNode, Result, Error};
+
+mod behaviour_graph;
