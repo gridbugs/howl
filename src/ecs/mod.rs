43c97b6ff33d133afcc5299dbbf15869811ef8bb
Stephen Sherratt <stephen@sherra.tt>
2016-11-12 14:25:18 +1100

    More tests for ecs

--- src/ecs/mod.rs
+++ src/ecs/mod.rs
@@ -1,5 +1,6 @@
 mod ecs;
-pub use self::ecs::{EcsTable, EcsCtx, EcsMap, EcsSet, EntityId, ComponentType, component_type, ComponentTypeSet};
+pub use self::ecs::{EcsTable, EcsCtx, EcsMap, EcsSet, EntityId, ComponentType, component_type, ComponentTypeSet,
+                    EntityRef, EntityRefMut};
 
 #[cfg(test)]
 mod tests;
--- src/ecs/tests.rs
+++ src/ecs/tests.rs
@@ -1,4 +1,6 @@
-use ecs::{ComponentTypeSet, component_type};
+use ecs::{ComponentTypeSet, component_type, EcsCtx};
+
+use geometry::Vector2;
 
 #[test]
 fn component_set() {
@@ -24,5 +26,34 @@ fn component_set_iter() {
     // components are iterated through in alphabetical order
     assert_eq!(iter.next(), Some(component_type::POSITION));
     assert_eq!(iter.next(), Some(component_type::SOLID));
-    assert_eq!(iter.next(), None);
+    assert!(iter.next().is_none());
+}
+
+#[test]
+fn entity_create_destroy() {
+    let mut ctx = EcsCtx::new();
+
+    let id = {
+        let mut entity = ctx.alloc_entity_mut();
+        entity.insert_solid();
+        entity.insert_position(Vector2::new(0, 0));
+
+        assert!(entity.contains_solid());
+        assert!(entity.contains_position());
+        assert!(!entity.contains_opacity());
+
+        assert_eq!(*entity.position().unwrap(), Vector2::new(0, 0));
+        assert!(!entity.is_empty());
+
+        entity.id()
+    };
+
+    ctx.remove_entity(id);
+
+    let entity = ctx.entity(id).unwrap();
+
+    assert!(!entity.contains_solid());
+    assert!(!entity.contains_position());
+    assert!(entity.position().is_none());
+    assert!(entity.is_empty());
 }
