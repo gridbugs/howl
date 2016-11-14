7c590c8eeff18e365093f883bc5e64282dbffeab
Stephen Sherratt <stephen@sherra.tt>
2016-11-14 23:59:36 +1100

    Added EntityId to ecs module

--- src/ecs/mod.rs
+++ src/ecs/mod.rs
@@ -1,9 +1,10 @@
 mod generated;
 
 // general ecs types/modules
-pub use self::generated::{EcsTable, EcsCtx, EcsAction, EcsActionProperties, ComponentTypeSet,
-                          ComponentTypeSetIter, ActionPropertyType, ActionPropertyTypeSetIter,
-                          EntityRef, EntityRefMut, component_type, action_property_type};
+pub use self::generated::{EntityId, EcsTable, EcsCtx, EcsAction, EcsActionProperties,
+                          ComponentTypeSet, ComponentTypeSetIter, ActionPropertyType,
+                          ActionPropertyTypeSetIter, EntityRef, EntityRefMut, component_type,
+                          action_property_type};
 
 #[cfg(test)]
 mod tests;
