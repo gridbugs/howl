03bff4f513d4d3b3f24054d4fad3e3a7faa1bd2b
Stephen Sherratt <stephen@sherra.tt>
2016-11-15 00:32:56 +1100

    Stripped old code from main file

--- src/main.rs
+++ src/main.rs
@@ -29,112 +29,9 @@ mod search;
 mod behaviour;
 mod ecs;
 
-use game::entities::*;
-use game::{EntityContext, ReserveEntityId, EntityId, GameContext, Level, EntityWrapper};
-use game::rules;
-use game::components::DoorState;
-
-use terminal::{Window, InputSource, WindowAllocator, BufferType, WindowBuffer};
-
 use std::io;
 
-fn populate(entities: &mut EntityContext) -> EntityId {
-    let strings = ["&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&",
-                   "&,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,&",
-                   "&,,############################,,,,,,&",
-                   "&,,#.........#................#,,&,,,&",
-                   "&,,#.........#................#,,,&,,&",
-                   "&,,#.........+................#,,&,,,&",
-                   "&&,#.........#................#,,,,,,&",
-                   "&,&#.........##########+#######,,,,,,&",
-                   "&,,#.........#,,,,,,,,,,,,,,,,,,,,,,,&",
-                   "&&,#.........#,d,,,,,,,&,,,,,,,&,&,&,&",
-                   "&,,#.........#,,,,,&,,,,,,,,&,,,,,,,,&",
-                   "&,,#.........+,,,,,,&,,,,,,,,,,,,,,,,&",
-                   "&&,#.........#,,,,,&,,,,,,,,,&,,,,,,,&",
-                   "&,,#.........#,,,,,,,,,,&,,&,,,&,&,,,&",
-                   "&,&#.........#,,,,@,,,,&,,,,,,,,,,,,,&",
-                   "&,,###########,,,,,,,&,,,,,,,&,&,,,,,&",
-                   "&,,&,,,,,,,,,,,,,,,,,&,,,,&,,,,,,,,,,&",
-                   "&,&,,,,,,,,,,,,&,,,,,,,,,,,,,,,,,,,,,&",
-                   "&,,,&,,,,,,,,,,,,,,,,&,,,,,#########,&",
-                   "&,&,,,&,,,,,&,,&,,,,&,,,,,,#.......#,&",
-                   "&,,,,,&,,,,,,,,,&,,,,&,,,,,#.......#,&",
-                   "&,,,,,,,,,&,,,,,,,,,,,,,&,,+.......#,&",
-                   "&,&,&,,,,&&,,,&,&,,,,,,,&,,#.......#,&",
-                   "&,,,,,,,,,,,,,,,,,,,&,,,,,,#.......#,&",
-                   "&,,,&,,,,,,,&,,,,,,,,,,,,,,#########,&",
-                   "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"];
-
-
-    let height = strings.len();
-    let width = strings[0].len();
-
-    let level_id = entities.reserve_level_id();
-    let mut level = Level::new(width, height, level_id);
-
-    let mut level_entities = Vec::new();
-    {
-        let mut y = 0;
-        for line in &strings {
-            let mut x = 0;
-            for ch in line.chars() {
-
-                let moonlight = !level.is_cloud(x, y);
-
-                match ch {
-                    '#' => {
-                        level_entities.push(make_wall(x, y));
-                        level_entities.push(make_floor(x, y));
-                    }
-                    '&' => {
-                        level_entities.push(make_tree(x, y));
-                        level_entities.push(make_floor_outside(x, y, moonlight));
-                    }
-                    '.' => {
-                        level_entities.push(make_floor(x, y));
-                    }
-                    ',' => {
-                        level_entities.push(make_floor_outside(x, y, moonlight));
-                    }
-                    '+' => {
-                        level_entities.push(make_door(x, y, DoorState::Closed));
-                        level_entities.push(make_floor(x, y));
-                    }
-                    '@' => {
-                        level_entities.push(make_pc(x, y));
-                        level_entities.push(make_floor_outside(x, y, moonlight));
-                    }
-                    'd' => {
-                        level_entities.push(make_dog(x, y));
-                        level_entities.push(make_floor_outside(x, y, moonlight));
-                    }
-                    _ => panic!(),
-                };
-
-                x += 1;
-            }
-            y += 1;
-        }
-    }
-
-    let mut pc = None;
-
-    for entity in level_entities.drain(..) {
-        let id = entities.reserve_entity_id();
-
-        if entity.is_pc() {
-            assert!(pc.is_none(), "Multiple player characters");
-            pc = Some(id);
-        }
-
-        level.add_external(id, entity, 0);
-    }
-
-    entities.add_level(level);
-
-    pc.expect("no player character found")
-}
+use terminal::{Window, InputSource, WindowAllocator, BufferType, WindowBuffer};
 
 const DEBUG_WINDOW_WIDTH: usize = 80;
 const DEBUG_WINDOW_HEIGHT: usize = 10;
@@ -156,20 +53,7 @@ fn window_session() {
     game(input, window);
 }
 
-fn game<'a>(input_source: InputSource, game_window: Window<'a>) {
-    let mut game_context = GameContext::new(input_source, game_window);
-
-    game_context.pc = Some(populate(&mut game_context.entities));
-
-    game_context.rule(rules::door::DetectOpen)
-        .rule(rules::collision::DetectCollision)
-        .rule(rules::axis_velocity::StartVelocityMovement)
-        .rule(rules::axis_velocity::MaintainVelocityMovement)
-        .rule(rules::burst_fire::BurstFireRule)
-        .rule(rules::transformation::BeastTransformation)
-        .rule(rules::transformation::HumanTransformation);
-
-    game_context.game_loop();
+fn game<'a>(_input_source: InputSource, _game_window: Window<'a>) {
 }
 
 fn make_debug_window<'a>(wa: &'a WindowAllocator, width: usize, height: usize) -> WindowBuffer<'a> {
