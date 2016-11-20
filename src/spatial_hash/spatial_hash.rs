993dd856d4c5f497317fa6f50002d6d5dd16ca70
Stephen Sherratt <stephen@sherra.tt>
2016-11-20 20:34:39 +1100

    Fleshed out spatial hash implementation

--- src/spatial_hash/spatial_hash.rs
+++ src/spatial_hash/spatial_hash.rs
@@ -65,24 +65,75 @@ impl SpatialHashTable {
         self.grid.get_mut_with_default(coord)
     }
 
+    fn change_entity_position(&mut self, entity: EntityRef,
+                              current_position: Coord,
+                              new_position: Coord) {
+
+    }
+
+    fn add_entity_position(&mut self, entity: EntityRef, position: Coord) {
+
+    }
+
+    fn remove_entity(&mut self, entity: EntityRef, position: Coord) {
+
+    }
+
+    fn remove_solid(&mut self, position: Coord) {
+
+    }
+
+    fn remove_opacity(&mut self, opacity: f64, position: Coord) {
+
+    }
+
     fn update_insertions(&mut self,
                          ctx: &EcsCtx,
                          insertions: &ActionInsertionTable,
                          insertion_types: &ComponentTypeSet) {
-
+        for (entity_id, new_position) in insertions.position.iter() {
+            let entity = ctx.entity(*entity_id);
+            if let Some(current_position) = entity.position() {
+                // the entity is changing position
+                self.change_entity_position(entity, current_position, *new_position);
+            } else {
+                // the entity is gaining a position
+                self.add_entity_position(entity, *new_position);
+            }
+        }
     }
 
     fn update_removals(&mut self,
                        ctx: &EcsCtx,
                        removals: &ActionRemovalTable,
                        removal_types: &ComponentTypeSet) {
+        for entity_id in removals.solid.iter() {
+            if let Some(position) = ctx.position(*entity_id) {
+                // removing solid from entity with position
+                self.remove_solid(position);
+            }
+        }
 
+        for entity_id in removals.opacity.iter() {
+            let entity = ctx.entity(*entity_id);
+            if let Some(position) = entity.position() {
+                if let Some(opacity) = entity.opacity() {
+                    // removing opacity from entity with position
+                    self.remove_opacity(opacity, position);
+                }
+            }
+        }
     }
 
     fn update_removed_entities(&mut self,
                                ctx: &EcsCtx,
                                entities: &HashSet<EntityId>) {
-
+        for entity_id in entities.iter() {
+            let entity = ctx.entity(*entity_id);
+            if let Some(position) = entity.position() {
+                self.remove_entity(entity, position);
+            }
+        }
     }
 
     pub fn update(&mut self, ctx: &EcsCtx, action: &EcsAction) {
