493042f369db30f2aafc5a688a5f92965b7eda7c
Stephen Sherratt <stephen@sherra.tt>
2017-01-02 22:45:14 +1100

    Removed CoordLine

--- src/coord/mod.rs
+++ src/coord/mod.rs
@@ -1,9 +1,7 @@
 mod coord;
-mod coord_line;
 mod straight_line;
 mod line_state;
 
 pub use self::coord::*;
-pub use self::coord_line::*;
 pub use self::straight_line::*;
 pub use self::line_state::*;
