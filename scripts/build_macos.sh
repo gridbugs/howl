af64d2912c89b52eba84b6f8cfcf7430b539a7a8
Stephen Sherratt <stephen@sherra.tt>
2017-01-14 15:46:10 +1100

    Removing .Trashes directory on macOS

--- scripts/build_macos.sh
+++ scripts/build_macos.sh
@@ -41,6 +41,7 @@ echo "Creating dmg"
 mkdir $MACOS_APP_NAME
 mv $MACOS_APP_DIR $MACOS_APP_NAME
 ln -s /Applications $MACOS_APP_NAME/Applications
+rm -rf $MACOS_APP_NAME/.Trashes
 FULL_NAME=$APP_NAME-$OS-$MACHINE-$SUFFIX
 hdiutil create uploads/$FULL_NAME.dmg -srcfolder $MACOS_APP_NAME -ov
 rm -rf $MACOS_APP_NAME
