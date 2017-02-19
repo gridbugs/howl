b12cf293a8aeea56ddd2c3f8a1736b51e1788224
Stephen Sherratt <stephen@sherra.tt>
2017-02-19 23:01:43 +1100

    Refactor save_file to use AsRef

--- src/game/save_file.rs
+++ src/game/save_file.rs
@@ -1,25 +1,25 @@
-use std::path;
-use std::fs;
+use std::path::Path;
+use std::fs::{self, File};
 use std::io::{Read, Write};
 use bincode::{self, SizeLimit};
 use game::*;
 
 const SAVE_FILE: &'static str = "save";
 
-pub fn save(user_path: &path::Path, game_state: GameState) -> GameState {
+pub fn save<P: AsRef<Path>>(user_path: P, game_state: GameState) -> GameState {
     let serializable = SerializableGameState::from(game_state);
 
     let encoded = bincode::serialize(&serializable, SizeLimit::Infinite).expect("Failed to serialize game state");
 
-    fs::File::create(user_path.join(SAVE_FILE))
+    File::create(user_path.as_ref().join(SAVE_FILE))
         .and_then(|mut f| f.write_all(&encoded))
         .expect("Failed to save game");
 
     GameState::from(serializable)
 }
 
-pub fn load(user_path: &path::Path) -> Option<GameState> {
-    if let Ok(mut f) = fs::File::open(user_path.join(SAVE_FILE)) {
+pub fn load<P: AsRef<Path>>(user_path: P) -> Option<GameState> {
+    if let Ok(mut f) = File::open(user_path.as_ref().join(SAVE_FILE)) {
         let mut encoded = Vec::new();
         f.read_to_end(&mut encoded).expect("Failed to read save file");
         let serializable: SerializableGameState = bincode::deserialize(&encoded).expect("Failed to parse save file");
@@ -29,6 +29,6 @@ pub fn load(user_path: &path::Path) -> Option<GameState> {
     }
 }
 
-pub fn delete(user_path: &path::Path) -> bool {
-    fs::remove_file(user_path.join(SAVE_FILE)).is_ok()
+pub fn delete<P: AsRef<Path>>(user_path: P) -> bool {
+    fs::remove_file(user_path.as_ref().join(SAVE_FILE)).is_ok()
 }
