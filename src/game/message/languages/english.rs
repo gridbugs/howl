7567f4c152834e630518817ad66a1c706284dd89
Stephen Sherratt <stephen@sherra.tt>
2017-03-03 02:32:16 +1100

    Remove duplication of control/input to string

--- src/game/message/languages/english.rs
+++ src/game/message/languages/english.rs
@@ -1,6 +1,5 @@
 use game::*;
 use colour::*;
-use direction::*;
 
 pub struct English;
 
@@ -53,67 +52,19 @@ impl English {
                 message.push(MessagePart::plain("Controls"));
             }
             MenuMessageType::Control(input, control) => {
-                if let Some(input_message) = self.translate_input_event(input) {
-                    self.translate_control(control, message);
-                    message.push(MessagePart::plain(": "));
-                    message.push(input_message);
-                }
+                message.push(MessagePart::Text(TextMessagePart::Plain(String::from(control))));
+                message.push(MessagePart::plain(": "));
+                message.push(MessagePart::Text(TextMessagePart::Plain(String::from(input))));
             }
             MenuMessageType::UnboundControl(control) => {
-                self.translate_control(control, message);
+                message.push(MessagePart::Text(TextMessagePart::Plain(String::from(control))));
                 message.push(MessagePart::plain(": (unbound)"));
             }
             MenuMessageType::ControlBinding(control) => {
-                self.translate_control(control, message);
+                message.push(MessagePart::Text(TextMessagePart::Plain(String::from(control))));
                 message.push(MessagePart::plain(": press a key..."));
             }
         }
-    }
-
-    fn translate_input_event(&self, input_event: InputEvent) -> Option<MessagePart> {
-        let message_part = match input_event {
-            InputEvent::Char(ch) => MessagePart::Text(TextMessagePart::Plain(format!("{}", ch))),
-            InputEvent::Up => MessagePart::plain("up"),
-            InputEvent::Down => MessagePart::plain("down"),
-            InputEvent::Left => MessagePart::plain("left"),
-            InputEvent::Right => MessagePart::plain("right"),
-            InputEvent::Escape => MessagePart::plain("esc"),
-            InputEvent::Return => MessagePart::plain("return"),
-            InputEvent::Space => MessagePart::plain("space"),
-            InputEvent::Quit => return None,
-        };
-
-        Some(message_part)
-    }
-
-    fn translate_direction(&self, direction: Direction) -> &'static str {
-        match direction {
-            Direction::North => "north",
-            Direction::South => "south",
-            Direction::East => "east",
-            Direction::West => "west",
-            Direction::NorthWest => "northwest",
-            Direction::SouthWest => "southwest",
-            Direction::NorthEast => "northeast",
-            Direction::SouthEast => "southeast",
-        }
-    }
-
-    fn translate_control(&self, control: Control, message: &mut Message) {
-        let string = match control {
-            Control::Direction(direction) => self.translate_direction(direction),
-            Control::Close => "close door",
-            Control::Fire => "fire",
-            Control::NextTarget => "next target",
-            Control::PrevTarget => "previous target",
-            Control::Wait => "wait a turn",
-            Control::DisplayMessageLog => "full screen message log",
-            Control::Examine => "examine",
-            Control::Pause => "pause",
-            Control::Use => "use",
-        };
-
-        message.push(MessagePart::plain(string));
     }
 
     fn translate_intro(&self, message: &mut Message) {
