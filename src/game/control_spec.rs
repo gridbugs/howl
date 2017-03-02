04a9bf265066a143d0b6828c566d7cc2ec99a7d9
Stephen Sherratt <stephen@sherra.tt>
2017-03-02 21:47:04 +1100

    Removed dead code

--- src/game/control_spec.rs
+++ src/game/control_spec.rs
@@ -111,66 +111,6 @@ impl<'a> From<&'a String> for InputEvent {
     }
 }
 
-fn parse_control(name: &str) -> Option<Control> {
-
-    let control = match name {
-        "North" => Control::Direction(Direction::North),
-        "South" => Control::Direction(Direction::South),
-        "East" => Control::Direction(Direction::East),
-        "West" => Control::Direction(Direction::West),
-        "Use" => Control::Use,
-        "Close" => Control::Close,
-        "Examine" => Control::Examine,
-        "Wait" => Control::Wait,
-        "Fire" => Control::Fire,
-        "NextTarget" => Control::NextTarget,
-        "PrevTarget" => Control::PrevTarget,
-        "DisplayMessageLog" => Control::DisplayMessageLog,
-        "Pause" => Control::Pause,
-        _ => return None,
-    };
-
-    Some(control)
-}
-
-fn parse_input_event(string: &str) -> Option<InputEvent> {
-    match string {
-        "Up" => return Some(InputEvent::Up),
-        "Down" => return Some(InputEvent::Down),
-        "Left" => return Some(InputEvent::Left),
-        "Right" => return Some(InputEvent::Right),
-        "Escape" => return Some(InputEvent::Escape),
-        "Return" => return Some(InputEvent::Return),
-        _ => {},
-    }
-
-    if string.len() == 1 {
-        if let Some(c) = string.chars().next() {
-            return Some(InputEvent::Char(c));
-        }
-    }
-
-    None
-}
-
-fn from_toml(table: toml::value::Table) -> Option<ControlMap> {
-    table.get("controls")
-        .and_then(|t| t.as_table())
-        .map(|controls| {
-            let mut control_map = ControlMap::new();
-
-            for (control_str, input_event_str) in controls.iter() {
-                if let Some(control) = parse_control(control_str.as_str()) {
-                    if let Some(input_event) = input_event_str.as_str().and_then(parse_input_event) {
-                        control_map.insert(input_event, control);
-                    }
-                }
-            }
-
-            control_map
-        })
-}
-
 pub fn from_file<P: AsRef<path::Path>>(path: P) -> Option<ControlMap> {
     let spec: Option<StringControlSpec> = game_file::read_toml(path).ok();
     spec.as_ref().map(ControlMap::from)
